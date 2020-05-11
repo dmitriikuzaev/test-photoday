78cc191781200de8ca0484eda02497fd1a2d1ddc
Dmitrii Kuzaev <dmitriikuzaev@yandex.ru>
2020-05-11 13:04:03 +0300

    init

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+const gulp = require("gulp");
+const less = require("gulp-less");
+const browserSync = require("browser-sync").create();
+
+const src_folder = "./src/";
+const src_assets_folder = "./src/assets/";
+const dist_folder = "./dist/";
+const dist_assets_folder = "./dist/assets/";
+
+gulp.task("html", () => {
+  return gulp
+    .src([src_folder + "**/*.html"], {
+      base: src_folder,
+      since: gulp.lastRun("html"),
+    })
+    .pipe(gulp.dest(dist_folder))
+    .pipe(browserSync.stream());
+});
+
+gulp.task("less", () => {
+  return gulp
+    .src([src_assets_folder + "less/**/*.less"], {
+      since: gulp.lastRun("less"),
+    })
+    .pipe(less())
+    .pipe(gulp.dest(dist_assets_folder + "css"))
+    .pipe(browserSync.stream());
+});
+
+gulp.task("serve", () => {
+  return browserSync.init({
+    server: {
+      baseDir: ["dist"],
+    },
+    port: 3000,
+    open: false,
+  });
+});
+
+gulp.task("watch", () => {
+  const watch = [src_folder + "**/*.html", src_assets_folder + "**/*.less"];
+  gulp.watch(watch, gulp.series("build")).on("change", browserSync.reload);
+});
+
+gulp.task("build", gulp.series("html", "less"));
+
+gulp.task("default", gulp.series("build", gulp.parallel("serve", "watch")));
